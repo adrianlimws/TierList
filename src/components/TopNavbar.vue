3c6526be932e934546596604d4a0b4ad01cb0b89
Adrian Lim <adrianlimws@gmail.com>
2024-12-26 12:36:19 +1300

    added remix

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,12 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logo from '../assets/tierlist.svg'
+</script>
 
 <template>
     <nav class="bg-white shadow">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex justify-between h-16 items-center">
-                <h1 class="text-xl font-bold text-gray-900">
-                    Source Tier List
-                </h1>
+            <div class="flex h-16 items-center">
+                <div class="h-8 w-8">
+                    <img
+                        :src="logo"
+                        alt="Logo"
+                        class="h-full w-full object-contain"
+                    />
+                </div>
+                <h1 class="text-xl font-bold text-gray-900">Tier List</h1>
             </div>
         </div>
     </nav>
