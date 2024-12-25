6dab97f14f14a59b6e4128ae4309dda98ddccecd
Adrian Lim <adrianlimws@gmail.com>
2024-12-26 12:07:04 +1300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TopNavBar from './components/TopNavbar.vue'
+</script>
+
+<template>
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <TopNavBar />
+        <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+            <h3 class="text-base font-semibold text-gray-900">Tier 1</h3>
+            <p class="mt-1 text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit quam
+                corrupti consectetur.
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <nav>
+        <h1>TierList</h1>
+    </nav>
+</template>
