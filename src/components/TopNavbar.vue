98814251b6f2ad65f3662e001a71a0a371664064
Adrian Lim <adrianlimws@gmail.com>
2024-12-26 13:30:45 +1300

    added original source for credits

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -5,7 +5,7 @@ import logo from '../assets/tierlist.svg'
 <template>
     <nav class="bg-white shadow">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex h-16 items-center">
+            <div class="flex h-16 items-center justify-between">
                 <div class="h-8 w-8">
                     <img
                         :src="logo"
@@ -14,6 +14,15 @@ import logo from '../assets/tierlist.svg'
                     />
                 </div>
                 <h1 class="text-xl font-bold text-gray-900">Tier List</h1>
+                <p class="text-sm text-gray-400">
+                    Modified from Original Source:
+                    <a
+                        class="underline hover:text-red-500"
+                        href="https://www.reddit.com/r/reddevils/wiki/transfer-reliability-guide/"
+                        target="_blank"
+                        >/r/reddevils</a
+                    >
+                </p>
             </div>
         </div>
     </nav>
